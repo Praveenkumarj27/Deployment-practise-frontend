cf776494a8511f7289ea50e4e06420d173fc8e22
praveenkumarj27 <jpraveenkumar263@gmail.com>
2022-09-26 09:36:35 +0530

    init

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -51,7 +51,7 @@ function Login() {
                 boxShadow:
                   "rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px",
                 margin: "0px 200px 0px 390px",
-                padding: "2px 10px 2px 60px",
+                padding: "2px 10px 2px 40px",
               }}
             >
               <div class="col-lg-9">
